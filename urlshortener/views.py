fc9a5d2a979ce010865787de9f0ec67e5d0bab69
Zmora-GH <mmdreg@gmail.com>
2020-07-08 00:49:16 +0300

    v1.0

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -1,23 +1,32 @@
-from flask import render_template, redirect, url_for, abort
+from flask import render_template, redirect, url_for, abort, request
 from app import app, db
 from forms import URLForm
 from models import ShortURL
 
-
 import string
 import random
 
-BASE_URL = 'localhost:5000/'
 
-def random_url(N=7):
+def random_url(N=8):
     slug = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(N))
     if db.session.query(ShortURL).filter_by(short_url=slug).first():
         return random_url
     return slug
 
 
+def get_urls(short_url):
+    url = db.session.query(ShortURL).filter_by(short_url=short_url).first()
+    if not url:
+        abort(404)
+    surl = request.host_url + url.short_url
+    furl = url.full_url
+    clicks = url.clicks
+    return surl, furl, clicks
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print(request.host_url)
     form = URLForm()
     if form.validate_on_submit():
         url = db.session.query(ShortURL).filter_by(full_url=form.url_string.data).first()
@@ -33,24 +42,24 @@ def index():
 
 @app.route('/<short_url>/clicks')
 def clicks(short_url):
-    #short_url == '' if >>> index else == valid short_url
-    if short_url:
-        pass
-    pass
+    surl, furl, clicks = get_urls(short_url)
+    return render_template('clicks.html', surl=surl, furl=furl, clicks=clicks)
+    form = ClickURLForm()
+
 
 
 @app.route('/short/<short_url>')
 def short_page(short_url):
-    url = db.session.query(ShortURL).filter_by(short_url=short_url).first()
-    surl = BASE_URL + url.short_url
-    furl = url.full_url
-    return render_template('short.html', surl=surl, furl=furl, short_url=url.short_url)
+    surl, furl, _ = get_urls(short_url)
+    return render_template('short.html', surl=surl, furl=furl, short_url=short_url)
 
 
 @app.route('/<short_url>')
 def redirecter(short_url):
     url = db.session.query(ShortURL).filter_by(short_url=short_url).first()
     if url:
+        url.clicks += 1
+        db.session.commit()
         return redirect(url.full_url)
     else:
         abort(404)
